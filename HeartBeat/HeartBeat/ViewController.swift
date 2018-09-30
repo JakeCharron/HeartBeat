6e5e2e5835469296ba3ac98ff17d9f051f73d3a0
Jake Charron <jcharron0306@gmail.com>
2018-09-30 18:15:49 -0400

    UserLoginChanges

--- HeartBeat/HeartBeat/ViewController.swift
+++ HeartBeat/HeartBeat/ViewController.swift
@@ -14,13 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     
+    var fireName = String()
+    var firePass = String()
+    
+    
     @IBAction func login(_ sender: Any) {
         var ref: DatabaseReference!
         ref = Database.database().reference()
-        print("\(username.text)")
-        ref.child("Users").child("\(username.text)").observeSingleEvent(of: .value) { (snapshot) in
-            print(snapshot.value)
+        ref.child("Users").observeSingleEvent(of: .value) { (snapshot) in
+            print("Users: ", snapshot)
+            var counter = 0
+            while (counter < snapshot.childrenCount){
+                
+                print("Child Snapshot: ", "\(String(describing: snapshot.childSnapshot(forPath: "\(counter)").value))")
+
+                let usernameVal = "\(String(describing: self.username.text))"
+                
+                let  fireVal = "\(String(describing: snapshot.childSnapshot(forPath: "\(counter)").value))"
+
+                if usernameVal == fireVal {
+                    print("\(String(describing: self.username.text)) ","equals ", " \(fireVal)")
+                }else{
+                    print("Username: \(usernameVal) is not equal to \(fireVal)")
+                }
+                counter += 1
+            }
         }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
     }
     
 }
