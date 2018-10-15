2b4152be37553dfcbaa87c51ad859be91528f6a1
Jake Charron <jcharron0306@gmail.com>
2018-10-15 00:19:14 -0400

    CAC Submission
    InfoScreen labels

--- HeartBeat/HeartBeat/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  HeartBeat
-//
-//  Created by JakeDev on 9/8/18.
-//  Copyright © 2018 Jake Charron. All rights reserved.
-//
-
-import Foundation
--- HeartBeat/MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-var str = "Hello, playground"
-
-var dict  = NSDictionary()
-
-var names = ["john", "adam", "henry"]
-
-
